8cb8b9a1dc5954b4063ed0aeeaedbdccee879f79
Maria-C <maria.n.cordoba@hotmail.com>
2025-08-02 19:55:36 -0300

    Primer commit - Hola mundo Spring Boot

---
+++ holaapp/src/test/java/com/ejemplo/holaapp/HolaappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplo.holaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolaappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
